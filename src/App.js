53bc08174fa183b07f556ec041ae9b1932733d81
Alex Lewis <alexlewis374@gmail.com>
2020-08-26 12:31:26 +0100

    Add RedocStandalone to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { RedocStandalone } from 'redoc'
 
 function App() {
   return (
     <div>
       <p>Hello</p>
+      <RedocStandalone specUrl="url/to/your/spec"/>
     </div>
   );
 }
