02c62589aebe73741306b20695ce5dac4ca1d11a
Alex Lewis <alexlewis374@gmail.com>
2020-08-26 14:23:54 +0100

    Remove unused files from public folder

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { RedocStandalone } from 'redoc'
 function App() {
   return (
     <div>
-      <p>Dave</p>
-      <RedocStandalone specUrl="https://raw.githubusercontent.com/AlexLewis10/users-api/master/swagger.json"/>
+      <RedocStandalone specUrl="https://raw.githubusercontent.com/AlexLewis10/users-api/master/swagger.json"
+        options={{
+        theme: { colors: { primary: { main: '#1E0D2D' } } }
+        }}
+      />
+      
     </div>
   );
 }
