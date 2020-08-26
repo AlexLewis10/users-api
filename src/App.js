6931bd3f22789abc77ed9ad2b9f8c488942c47b6
Alex Lewis <alexlewis374@gmail.com>
2020-08-26 12:39:35 +0100

    Add swagger.json url to redocly component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { RedocStandalone } from 'redoc'
 function App() {
   return (
     <div>
-      <p>Hello</p>
-      <RedocStandalone specUrl="url/to/your/spec"/>
+      <p>Dave</p>
+      <RedocStandalone specUrl="https://raw.githubusercontent.com/AlexLewis10/users-api/master/swagger.json"/>
     </div>
   );
 }
