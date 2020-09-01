1cdc9f7db1e0b419c61e08a197a49340262ad202
Alex Lewis <alexlewis374@gmail.com>
2020-09-01 10:18:42 +0100

    Remove double quotes and replace with single quotes

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -4,7 +4,7 @@ import { RedocStandalone } from 'redoc'
 const Requests = () => {
   return (
     <RedocStandalone 
-      specUrl="https://raw.githubusercontent.com/AlexLewis10/users-api/master/swagger.json"
+      specUrl='https://raw.githubusercontent.com/AlexLewis10/users-api/master/swagger.json'
       options={{
         theme: { 
           colors: { primary: { main: '#320B42' }} 
